2a420082c20c2ed7972f5a6f5f300526826dd30b
Travis Callahan <callahantravis29@gmail.com>
2019-02-07 16:32:42 -0600

    Added edit and delete buttons.

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,17 +1,3 @@
-let coll = $('.collapsible')
-
-for (let i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    let content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
-}
-
 let modal = $('.modal');
 let modalBtn = $('#new-bill');
 let closeBtn = $('.closeBtn');
@@ -36,7 +22,60 @@ function outsideClick(e) {
     }
 }
 
-$('form').on('click', '#submit-button', function(event) {
-  event.preventDefault();
-  closeModal();
-});
+function handleCollapsible() {
+ $('#dashboard-body').on('click', '.collapsible', function() {
+      this.classList.toggle("active");
+      let content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      } 
+    });
+  }
+
+function renderData(data) {
+  $('.js-bill-list').append(
+    `
+    <li class="js-bill-item">
+    <button class="collapsible">${data.billName}</button>
+    <div class="content">
+        <p>Due date: ${data.dueDate}</p>
+        <p>Amount: ${data.billAmount}</p>
+        <p>Bill Website: ${data.billWebsite}</p>
+        <button class="js-bill-item-delete">Delete</button>
+        <button class="js-bill-item-edit">Edit</button>
+    </div>
+    </li>`
+  );
+}
+
+function handleSubmit() {
+  $('form').on('click', '#submit-button', function(event) {
+    event.preventDefault();
+    const data = {
+      billName: $('#billName').val(),
+      billAmount: $('#billAmount').val(),
+      billWebsite: $('#billWebsite').val(),
+      dueDate: $('#date-input').val()
+    }
+    console.log(data);
+    renderData(data)
+    closeModal();
+  });
+}
+
+function handleBillDelete() {
+  $('.js-bill-list').on('click', '.js-bill-item-delete', function(e) {
+    e.preventDefault();
+    $(e.currentTarget).closest('.js-bill-item').remove();
+  });
+}
+
+function handleApp() {
+  handleCollapsible();
+  handleSubmit();
+  handleBillDelete();
+}
+$(handleApp);
+
